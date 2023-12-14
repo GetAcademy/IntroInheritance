5282d374488363263575fba621218538b51875ab
GET Academy <terje@kolderup.net>
2023-12-14 10:22:54 +0100

    x

---
+++ IntroInheritance.WPF/ExitButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace IntroInheritance.WPF
+{
+    internal class ExitButton : Button
+    {
+        public ExitButton()
+        {
+            var borderStyle = new Style(typeof(Border));
+            borderStyle.Setters.Add(new Setter(Border.CornerRadiusProperty, new CornerRadius(20)));
+            Resources.Add(typeof(Border), borderStyle);
+            Background = Brushes.Red;
+            Foreground = Brushes.White;
+            Width = 50;
+            Height = 50;
+            Content = "EXIT";
+        }
+    }
+}
--- IntroInheritance.WPF/Program.cs
+++ IntroInheritance.WPF/Program.cs
@@ -6,12 +6,13 @@ namespace IntroInheritance.WPF
     internal class Program
     {
         [STAThread]
-        public void Main(string[] args)
+        public static void Main(string[] args)
         {
             var app = new Application();
             var window = new Window();
             var panel = new StackPanel();
             window.Content = panel;
+            panel.Children.Add(new ExitButton());
             app.Run(window);
         }
     }
