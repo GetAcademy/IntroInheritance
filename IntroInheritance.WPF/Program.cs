113528b92242a5493e0c97a8d6ae219f9d93fd82
GET Academy <terje@kolderup.net>
2023-12-14 09:25:55 +0100

    Initial commit

---
+++ IntroInheritance.WPF/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace IntroInheritance.WPF
+{
+    internal class Program
+    {
+        [STAThread]
+        public void Main(string[] args)
+        {
+            var app = new Application();
+            var window = new Window();
+            var panel = new StackPanel();
+            window.Content = panel;
+            app.Run(window);
+        }
+    }
+}
